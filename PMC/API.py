8f96780725bcbe859e90650cb3983924b71dc334
shernandezf <s.hernandezf@uniandes.edu.co>
2022-05-04 23:32:22 -0500

    se ha creado el proyecto

---
+++ PMC/API.py
@@ -0,0 +1,39 @@
+from typing import List
+from pydantic import BaseModel
+from fastapi import FastAPI
+import pandas as pd
+from joblib import load
+import numpy as np
+from sklearn.metrics import mean_squared_error as mse
+
+app = FastAPI()
+class DataModelapp(BaseModel):
+    temperature: float
+    humidity: float
+    ph: float
+class ListPrediccion(BaseModel):
+    datos:List[DataModelapp]=None
+@app.get("/")
+def read_root():
+   return {"Hello": "World"}
+
+@app.post("/Data/predict")
+async def make_prediction(dataModel: DataModelapp):
+    
+    df = pd.DataFrame(dataModel.dict(),index=[0])
+    model = load("pipeline.joblib")
+    result = model.predict(df)
+    resultado=result[0]
+    return {"Tiempo de expectativa de vida: ": resultado}
+
+@app.post("/Data/predict/list")
+async def make_predictionsList(dataList: ListPrediccion):
+    lista_datos=dataList.dict()['datos']
+    model = load("pipeline.joblib")
+    respuestastr=''
+    for i in lista_datos:
+        df = pd.DataFrame(i,index=[0])
+        result = model.predict(df)
+        resultado=result[0]
+        respuestastr=respuestastr+' , '+str(resultado)
+    return {"Tiempos de expectativa de vida de cada set de datos: ": respuestastr}
